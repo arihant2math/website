7aa313086fa40366179a942711740f4cab2a3c98
Ashwin <arihant2math@gmail.com>
2020-06-12 13:20:58 -0700

    style

--- flaskr/download.py
+++ flaskr/download.py
@@ -1,4 +1,11 @@
-from flask import Blueprint
+from flask import (
+    Blueprint, render_template
+)
 
 
 bp = Blueprint('download', __name__, url_prefix='/download')
+
+
+@bp.route('/index')
+def register():
+    return render_template('download/index.html')
--- flaskr/home.py
+++ flaskr/home.py
@@ -1,3 +1,10 @@
-from flask import Blueprint
+from flask import (
+    Blueprint, render_template
+)
 
 bp = Blueprint('home', __name__, url_prefix='/home')
+
+
+@bp.route('/index')
+def register():
+    return render_template('home/index.html')
